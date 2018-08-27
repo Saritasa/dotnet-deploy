60acb4687616b8f57bbfd5ca0c537ae9752359ad
Anton Zimin <anton@saritasa.com>
2018-08-27 22:08:05 +0700

    Introduce Windows Service project #1

---
+++ src/DeployDemo.Service/DemoService.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace DeployDemo.Service
+{
+    public partial class DemoService : ServiceBase
+    {
+        public DemoService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
