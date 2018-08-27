7ad363a9a76db2b2c7466e2bbb6da4a4d4fdaee2
Anton Zimin <anton@saritasa.com>
2018-08-27 22:23:53 +0700

    Implement App.configs transformations, show environment #2

--- src/DeployDemo.Service/DemoService.cs
+++ src/DeployDemo.Service/DemoService.cs
@@ -1,4 +1,6 @@
-﻿using NLog;
+﻿using DeployDemo.Service.Support;
+using Microsoft.Extensions.Hosting;
+using NLog;
 using System.ServiceProcess;
 
 namespace DeployDemo.Service
@@ -14,6 +16,11 @@ namespace DeployDemo.Service
 
         protected override void OnStart(string[] args)
         {
+            var environment = new AppHostingEnvironment();
+
+            logger.Info("DemoService started.");
+            logger.Info($"Environment: {environment.EnvironmentName}");
+            logger.Info($"Production: {environment.IsProduction()}");
         }
 
         protected override void OnStop()
---
+++ src/DeployDemo.Service/Support/AppHostingEnvironment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using System.Configuration;
+using System.IO;
+using System.Reflection;
+
+namespace DeployDemo.Service.Support
+{
+    public class AppHostingEnvironment : IHostingEnvironment
+    {
+        private readonly string environment;
+        private readonly string applicationName;
+        private readonly string contentRootPath;
+
+        public AppHostingEnvironment()
+        {
+            environment = ConfigurationManager.AppSettings["Environment"];
+
+            var entryAssembly = Assembly.GetEntryAssembly();
+            applicationName = entryAssembly.GetName().Name;
+            contentRootPath = Path.GetDirectoryName(entryAssembly.Location);
+        }
+
+        public string EnvironmentName { get => environment; set => throw new System.NotImplementedException(); }
+        public string ApplicationName { get => applicationName; set => throw new System.NotImplementedException(); }
+        public string ContentRootPath { get => contentRootPath; set => throw new System.NotImplementedException(); }
+        public IFileProvider ContentRootFileProvider { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    }
+}
