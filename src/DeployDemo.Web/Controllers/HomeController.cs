6249a16722f57bf1ec10ce5d95703899d75a4f4a
Anton Zimin <anton@saritasa.com>
2018-08-28 11:06:19 +0700

    Show environment in ASP.NET Core project #2

--- src/DeployDemo.Web/Controllers/HomeController.cs
+++ src/DeployDemo.Web/Controllers/HomeController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DeployDemo.Web.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DeployDemo.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index([FromServices] IHostingEnvironment hostingEnvironment)
         {
-            return View();
+            var info = new InfoModel
+            {
+                Environment = hostingEnvironment.EnvironmentName,
+                IsProduction = hostingEnvironment.IsProduction(),
+            };
+
+            return View(info);
         }
     }
 }
